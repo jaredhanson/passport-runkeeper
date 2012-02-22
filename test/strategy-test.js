8ecc6cc719c9e92eaefd0a58f2cf64545c1cc56a
Jared Hanson <jaredhanson@gmail.com>
2012-02-22 08:28:51 -0800

    Conform to latest conventions.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -29,7 +29,7 @@ vows.describe('RunKeeperStrategy').addBatch({
       function() {});
       
       // mock
-      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+      strategy._oauth2.get = function(url, accessToken, callback) {
         var body = '{ \
         "userID": 1234567890, \
         "profile": "/profile", \
@@ -71,6 +71,12 @@ vows.describe('RunKeeperStrategy').addBatch({
         assert.equal(profile.provider, 'runkeeper');
         assert.equal(profile.id, '1234567890');
       },
+      'should set raw property' : function(err, profile) {
+        assert.isString(profile._raw);
+      },
+      'should set json property' : function(err, profile) {
+        assert.isObject(profile._json);
+      },
     },
   },
   
@@ -83,7 +89,7 @@ vows.describe('RunKeeperStrategy').addBatch({
       function() {});
       
       // mock
-      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+      strategy._oauth2.get = function(url, accessToken, callback) {
         callback(new Error('something-went-wrong'));
       }
       
